b54df5cb886ce4210a1a3580116e085cda45bca7
Grant Horner <h.grant.horner@gmail.com>
2021-12-03 20:55:08 -0500

    update .gitignore and doc strings

--- src/day_3.clj
+++ src/day_3.clj
@@ -1,8 +1,10 @@
 (ns day-3
   (:require [clojure.string :as str]))
 
-(defn most-common-bit [input n]
+(declare puzzle-input sample)
+(defn most-common-bit
   "Determines the most common bit in a particular position in a sequence of strings of bits."
+  [input n]
   (let [m (reduce (fn [acc x]
                     (update acc (Integer/parseInt (str (nth x n))) inc))
                   {0 0, 1 0}
@@ -11,8 +13,9 @@
       0
       1)))
 
-(defn least-common-bit [input n]
+(defn least-common-bit
   "Determines the least common bit in a particular position in a sequence of strings of bits."
+  [input n]
   (let [m (reduce (fn [acc x]
                     (update acc (Integer/parseInt (str (nth x n))) inc))
                   {0 0, 1 0}
