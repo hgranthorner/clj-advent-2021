0bc83eb68667a799df96476b8b1a0c911da6bda2
Grant <h.grant.horner@gmail.com>
2021-12-04 22:21:21 -0500

    create series from board

--- src/day_4.clj
+++ src/day_4.clj
@@ -33,18 +33,48 @@
   (->> input
        str/split-lines
        (into [] group-boards-xf)
-       (#(hash-map :moves (first %) :board-strings (rest %)))
+       (#(hash-map :moves (str/split (get-in % [0 0]) #"\,") :board-strings (rest %)))
        ((fn [{:keys [board-strings] :as m}]
           (assoc m :boards
-                 (map (partial mapcat board-string->maps) board-strings))))))
+                 (mapv (partial into [] (mapcat board-string->maps)) board-strings))))))
+
+(defn mark-board-from-move
+  "When given a move (a string of an integer), marks the board's cells as marked if the value equals the move."
+  [move board]
+  (mapv #(if (= (:value %) move)
+           (assoc % :marked true)
+           %) board))
+
+(defn get-rows
+  "When given a set of 25 cells, creates rows."
+  [board]
+  (map
+   #(subvec board (* (dec %) 5) (* % 5))
+   (range 1 6)))
+
+(defn get-columns
+  "When given a set of 25 cells, creates columns."
+  [board]
+  (map #(vals (select-keys board (map (partial * %) [0 5 10 15 20]))) (range 1 6)))
+
+(defn get-diagonals
+  "When given a set of 25 cells, creates diagonals."
+  [board]
+  [(vals (select-keys board [5 9 13 17 21])) (vals (select-keys board [0 6 12 18 14] #_(map (partial * 6) [0 1 2 3 4])))])
 
 (comment
+  (let [data (parse-input (slurp "inputs/day_4_sample.txt"))
+        boards (:boards data)
+        move (first (:moves data))
+        marked-boards (mapv (partial mark-board-from-move move) boards)]
+    (def *marked-boards marked-boards))
   
-  ;; => ({:marked false, :value "8"} {:marked false, :value "2"} {:marked false, :value "23"} {:marked false, :value "4"} {:marked false, :value "24"})
-
-
-  ;; => ["" "8" "" "2" "23" "" "4" "24"]
-
+  ; working on determining a winner
+  (let [board (first *marked-boards)
+        series (concat (get-rows board) (get-columns board) (get-diagonals board))]
+    (->> series
+         (map (partial map :marked))
+         (filter (partial every? identity))))
   )
 ;; => {:moves ["7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1"], 
 ;;     :board-strings (["22 13 17 11  0"
