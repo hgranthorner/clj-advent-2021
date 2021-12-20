9dc56bfeb5708c32782467f49c23a5d7651b7b45
Grant Horner <h.grant.horner@gmail.com>
2021-12-20 00:28:53 -0500

    completed day 5

--- src/day_5.clj
+++ src/day_5.clj
@@ -29,31 +29,38 @@
      :diagram (repeat (* (inc height) (inc height)) 0)}))
 
 (defn inclusive-range
-   [min max]
-   (if (= min max)
-     (list min)
-     (range min (inc max))))
- 
+  [min max]
+  (if (= min max)
+    (list min)
+    (if (< min max)
+      (range min (inc max))
+      (range min (dec max) -1))))
+
 (defn coordinate-cross-product
-   [{:keys [x1 x2 y1 y2]}]
-   (for [x (inclusive-range (min x1 x2)
-                            (max x1 x2))
-         y (inclusive-range (min y1 y2)
-                            (max y1 y2))]
-     [x y]))
+  [{:keys [x1 x2 y1 y2]}]
+  (if (or (= y1 y2)
+          (= x1 x2))
+    (for [x (inclusive-range (min x1 x2)
+                             (max x1 x2))
+          y (inclusive-range (min y1 y2)
+                             (max y1 y2))]
+      [x y])
+    (map vector
+         (inclusive-range x1 x2)
+         (inclusive-range y1 y2))))
 
 
 (let [xs [1 2 3]
       ys [0 0 0 0 0 0 0]]
-  (map-indexed #(if ((set xs) %1) (inc %2) %2) ys))
+  (map-indexed #(if ((set xs) %1) (inc %2) %2) ys)
 
- (defn mark-diagram
+  (defn mark-diagram
     "Returns a new diagram.
       Marks the diagram with the coordinates, and the space between the coordinates."
     [diagram width coordinate]
     (let [cp (coordinate-cross-product coordinate)
           points (->> cp (map (fn [[x y]] (+ x (* y (inc width))))) set)]
-      (map-indexed #(if (points %1) (inc %2) %2) diagram)))
+      (map-indexed #(if (points %1) (inc %2) %2) diagram))))
 
 (defn solution-one
   [input]
@@ -70,18 +77,41 @@
                    (update :diagram mark-diagram (:width diagram) coordinate)))
         (count (filter #(< 1 %) (:diagram diagram)))))))
 
-   (comment
-     (solution-one "inputs/day_5_sample.txt")
-     (solution-one "inputs/day_5_input.txt")
-     (def diagram (->> (parse-input "inputs/day_5_sample.txt")
-                       (filter #(or (= (:y1 %) (:y2 %))
-                                    (= (:x1 %) (:x2 %))))
-                       build-diagram))
+(defn mark-diagram-diagonal
+  "Returns a new diagram.
+   Marks the diagram with the coordinates, and the space between the coordinates."
+  [diagram width coordinate]
+  (let [cp (coordinate-cross-product coordinate)
+        points (->> cp (map (fn [[x y]] (+ x (* y (inc width))))) set)]
+    (map-indexed #(if (points %1) (inc %2) %2) diagram)))
 
-     (def coordinate {:y1 4, :x1 3, :y2 4, :x2 1})
+(defn solution-two
+  [input]
+  (let [diagram (->> (parse-input input)
+                     build-diagram)]
+    (loop [coordinate (first (:coordinates diagram))
+           diagram (update diagram :coordinates rest)]
+      (if coordinate
+        (recur (first (:coordinates diagram))
+               (-> diagram
+                   (update :coordinates rest)
+                   (update :diagram mark-diagram (:width diagram) coordinate)))
+        (count (filter #(< 1 %) (:diagram diagram)))))))
 
-     (coordinate-cross-product coordinate)
 
 
-     (re-matches #"^(\d+),(\d+) -> (\d+),(\d+)" "5,5 -> 8,2")
-     )
+(comment
+  (solution-one "inputs/day_5_sample.txt")
+  (future (prn (solution-two "inputs/day_5_input.txt")))
+  (future (prn (solution-one "inputs/day_5_input.txt")))
+  (def diagram (->> (parse-input "inputs/day_5_sample.txt")
+                    (filter #(or (= (:y1 %) (:y2 %))
+                                 (= (:x1 %) (:x2 %))))
+                    build-diagram))
+
+  (def coordinate {:y1 4, :x1 3, :y2 4, :x2 1})
+
+  (coordinate-cross-product coordinate)
+
+
+  (re-matches #"^(\d+),(\d+) -> (\d+),(\d+)" "5,5 -> 8,2"))
