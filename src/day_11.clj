f62bf0f773943b383646a2b75b12ed661cad7e25
Grant <h.grant.horner@gmail.com>
2021-12-30 20:21:10 -0500

    extracted function

--- src/day_11.clj
+++ src/day_11.clj
@@ -4,7 +4,6 @@
             [clojure.string :as str]
             [clojure.test :refer [deftest testing is run-tests]]
             [malli.core :as m]
-            [malli.error :as me]
             [malli.registry :as mr]
             [malli.util :as mu]
             [malli.instrument :as mi]
@@ -16,17 +15,17 @@
   "Parses input into a height map."
   [input]
   (->> input
-    str/split-lines
-    (mapv #(str/split % #""))
-    (sp/transform [sp/ALL sp/ALL] #(Integer/parseInt %))
-    (sp/transform [sp/ALL]
-      #(map-indexed
-         (fn [x y] (sorted-map x {::brightness y ::x x ::flashed? false})) %))
-    (map #(into (sorted-map) %))
-    (map-indexed
-      (fn [x y]
-        [x (sp/transform [sp/MAP-VALS] #(assoc % ::y x) y)]))
-    (into (sorted-map))))
+       str/split-lines
+       (mapv #(str/split % #""))
+       (sp/transform [sp/ALL sp/ALL] #(Integer/parseInt %))
+       (sp/transform [sp/ALL]
+                     #(map-indexed
+                        (fn [x y] (sorted-map x {::brightness y ::x x ::flashed? false})) %))
+       (map #(into (sorted-map) %))
+       (map-indexed
+         (fn [x y]
+           [x (sp/transform [sp/MAP-VALS] #(assoc % ::y x) y)]))
+       (into (sorted-map))))
 
 (defn inc-round
   "`inc` input - if the number is gt 9 then round down to 10."
@@ -41,21 +40,22 @@
 
 (def adjacent-coordinates
   (remove #(= (first %) (second %) 0)
-    (for [x (range -1 2)
-          y (range -1 2)]
-      [x y])))
+          (for [x (range -1 2)
+                y (range -1 2)]
+            [x y])))
 
 (defn adjacent-octopi
   [grid {::keys [x y]}]
   (remove nil?
-    (for [[xd yd] adjacent-coordinates]
-      (get-in grid [(+ yd y) (+ xd x)]))))
+          (for [[xd yd] adjacent-coordinates]
+            (get-in grid [(+ yd y) (+ xd x)]))))
 
 (defn mark-flashed
   "Marks an octopus as 'flashed' if its brightness is 0."
   [{::keys [brightness] :as o}]
   (if (zero? brightness)
-    (assoc o ::flashed? true)
+    (assoc o ::flashed? true
+             ::just-flashed? true)
     o))
 
 (defn grow-older
@@ -69,29 +69,41 @@
   [grid]
   (sp/transform [sp/MAP-VALS sp/MAP-VALS] grow-older grid))
 
+(defn determine-octopi-to-increase
+  [grid flashed-octopi]
+  (into #{}
+        (comp
+          (mapcat (partial adjacent-octopi grid))
+          (filter #(not (::flashed? %)))
+          (map #(select-keys % [::x ::y])))
+        flashed-octopi))
+
 (defn increase-surrounding-energy
   "Find the flashed octopi and increase the surrounding octopi's brightness."
   [grid]
-  (let [flashed (sp/select [sp/MAP-VALS sp/MAP-VALS #(::flashed? %)] grid)
-        to-increase (into #{}
-                      (comp
-                        (mapcat (partial adjacent-octopi grid))
-                        (filter #(not (::flashed? %)))
-                        (map #(select-keys % [::x ::y])))
-                      flashed)]
+  (let [grid grid
+        just-flashed (sp/select [sp/MAP-VALS sp/MAP-VALS #(::just-flashed? %)] grid)
+        ; Only age the octopi that haven't flashed yet
+        to-increase (determine-octopi-to-increase grid just-flashed)]
     (sp/transform
       [sp/MAP-VALS sp/MAP-VALS #(to-increase (select-keys % [::x ::y]))]
       grow-older
       grid)))
 
 (def grid (parse-input (slurp "inputs/day_11_sample.txt")))
-
+(def mini-grid (parse-input "11111\n19991\n19191\n19991\n11111"))
 (increase-surrounding-energy
   (-> grid pass-time pass-time))
+
+(-> mini-grid
+    pass-time
+    increase-surrounding-energy)
 
 (adjacent-octopi grid {::x 1 ::y 1})
 
 ; Two phases
-; 1 - time passes. octopi brightness all goes up. If anyone rolls over they're marked as "flashed"
-; 2 - find every octopus that flashed and increase the adjacent octopus's brightness. Remove their flashed flag.
-;   - This step can be continuous, as one octopus flashing may make another octopus flash.
+; 1 - time passes. octopi brightness all goes up. If anyone rolls over they're marked as "flashed" to prevent from flashing again,
+;     and "just-flashed" to indicate that the surrounding octopi need to grow-older.
+; 2 - find every octopus that just-flashed and increase the adjacent octopus's brightness. Remove their just-flashed flag.
+;   - This may cause other octopi to flash - continue to find the "just-flashed" octopi until there are none left
+; 3 - Set all "flashed" octopi to 0 brightness and remove their flashed and just-flashed statuses.
